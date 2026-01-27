d724b91bbec3566624d7a888c62c2a2ddb0272a7
Randima <randimafernando1997@gmail.com>
2026-01-27 18:19:57 +0100

    Construct test train split function

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Function splits the test and train sets accordingly
+def train_test_split_userwise(df, test_size = 0.2):
+    train, test = [], []
+    for user, group in df.groupby("userId"):            # Returns interator which at each point give a tuple of key and subgroup
+        if len(group) >= 5:                             # Makes sure users that haven't rated much do not get included in test set
+                        # Line below splits each set of ratings by a user into test and train sets
+            tr, te = train_test_split(group, test_size= test_size, random_state= 42)
+            train.append(tr)
+            test.append(te)
+        else:
+            train.append(group)                         # Users with fewer than 5 ratings simply added here for training
+    
+    return pd.concat(train), pd.concat(test)            # Concatenates the list elements into a single pandas dataframe
+
 
 ratings = pd.read_csv('Movie Lens/ml-32m/ratings.csv')
 #movies = pd.read_csv('Movie Lens/ml-32m/movies.csv')
@@ -13,6 +29,7 @@ sparsity = len(ratings)/(n_users*n_movies)
 
 print(n_users, n_movies, sparsity, len(ratings))
 
+
 # Make a User-Item matrix, with Users on one axis and Movies on the other
 user_item = ratings.pivot(
     index= 'userId',
@@ -20,7 +37,19 @@ user_item = ratings.pivot(
     values= 'rating'
 )
 
-print(user_item.iloc[0:10, 0:10])
 
+# The following figures provide a unpersonalized average rating figure to be treated as a baseline
+# Performance worse than the below suggests that there's something wrong with the model
+global_mean = ratings['rating'].mean()
+
+movie_stats = (ratings.groupby('movieId')
+               .agg(mean_rating = ('rating', 'mean'), count = ('rating', 'count')))
+
+
+
+
+
+
+print(user_item.iloc[0:10, 0:10], "\n \n", movie_stats)
 #print(n_users, n_movies, '\n \n', ratings_2.head())
 
